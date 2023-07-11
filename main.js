70e14d909e332c7cb76d0c59281ce63ab5cf89fb
Julio Rivas <rivasjulio@ymail.com>
2023-07-11 19:32:24 -0400

    completed

--- main.js
+++ main.js
@@ -3,8 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById('modal');
+const errorMessage = document.getElementById('modal-message');
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  errorModal.classList.add('.hidden');
+});
+
+document.addEventListener('click', (event) => {
+  const heart = event.target;
+  if (heart.classList.contains('like-glyph')) {
+    mimicServerCall()
+      .then(() => {
+        heart.classList.toggle('activated-heart');
+        heart.classList.toggle('full-heart');
+      })
+      .catch(() => {
+        errorMessage.textContent = 'Server Error. Please try again.';
+        errorModal.classList.remove('.hidden');
+        setTimeout(() => {
+          errorModal.classList.add('.hidden');
+        }, 3000);
+      });
+  } else if (heart.classList.contains('activated-heart')) {
+    heart.classList.toggle('activated-heart');
+    heart.classList.toggle('full-heart');
+  }
+});
+
+describe("main.js", () => {
+  it("contains a hidden modal", () => {
+    let modal = document.querySelector('.hidden');
+    expect(modal).to.not.equal(null);
+  });
+});
 
 
 //------------------------------------------------------------------------------
